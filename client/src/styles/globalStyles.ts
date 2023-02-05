4372cb23ae4aa5c81da7bd527cf866a8acb48452
Samuel Candela <93607710+samc18@users.noreply.github.com>
2023-02-05 15:43:21 -0500

    feat: add header

---
+++ client/src/styles/globalStyles.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *, *::before, *::after {
+        box-sizing: border-box;
+    }
+
+    * {
+        margin: 0;
+    }
+
+    html, body {
+        height: 100%;
+    }
+
+    body {
+        line-height: 1.5;
+        font-family: 'Dameron', serif;
+    }
+
+    img, picture, video, canvas, svg {
+        display: block;
+        max-width: 100%;
+    }
+
+    input, button, textarea, select {
+        font: inherit;
+    }
+    
+    @font-face {
+        src: url('/fonts/dameron.ttf');
+        font-family: 'Dameron';
+    }
+
+    @font-face {
+        src: url('/fonts/dameron-bold.ttf');
+        font-family: 'Dameron Bold';
+    }
+`
+
+export default GlobalStyle
---
+++ client/src/styles/stylesVariables.ts
@@ -0,0 +1,19 @@
+export const colors: any = {
+    accent: '#dc070d',
+    background: '#201e1e',
+    font: '#fff',
+}
+
+export const fonts: any = {
+    regular: 'Dameron',
+    bold: 'Dameron Bold',
+}
+
+export const margins: any = {
+    mobile: '20px',
+    desktop: '50px',
+}
+
+export const fontSizes: any = {
+    300: '1.5rem',
+}
