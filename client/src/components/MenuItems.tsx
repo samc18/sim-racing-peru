4dceef2c084bc06f6bb9ec6c50e9fca38191cc6c
Samuel Candela <93607710+samc18@users.noreply.github.com>
2023-02-05 22:39:32 -0500

    refactor: update menu items components

---
+++ client/src/components/MenuItems.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import Item from './Item'
+
+const StyledMenuItems = styled.div`
+    padding: 3em 0;
+    display: grid;
+    gap: 3em;
+`
+
+const MenuItems = () => {
+    return (
+        <StyledMenuItems>
+            <Item text='Competiciones' bgUrl='../../public/images/home/events.jpg' />
+            <Item text='Herramientas' bgUrl='../../public/images/home/tools.png' />
+            <Item text='Pilotos' bgUrl='../../public/images/home/drivers.png' />
+            <Item text='Equipos' bgUrl='../../public/images/home/teams.png' />
+            <Item text='Pits' bgUrl='../../public/images/home/pits.jpg' />
+        </StyledMenuItems>
+    )
+}
+
+export default MenuItems
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { colors } from '../styles/stylesVariables'
-import Item from '../components/Item'
+import MenuItems from '../components/MenuItems'
 
 const StyledHome = styled.div`
     background-image: url(../../public/images/home/background.jpg);
@@ -8,22 +8,10 @@ const StyledHome = styled.div`
     background-blend-mode: multiply;
 `
 
-const ItemsContainer = styled.div`
-    padding: 3em 0;
-    display: grid;
-    gap: 3em;
-`
-
 const Home = () => {
     return (
         <StyledHome>
-            <ItemsContainer>
-                <Item text='Competiciones' bgUrl='../../public/images/home/events.jpg' />
-                <Item text='Herramientas' bgUrl='../../public/images/home/tools.png' />
-                <Item text='Pilotos' bgUrl='../../public/images/home/drivers.png' />
-                <Item text='Equipos' bgUrl='../../public/images/home/teams.png' />
-                <Item text='Pits' bgUrl='../../public/images/home/pits.jpg' />
-            </ItemsContainer>
+            <MenuItems />
         </StyledHome>
     )
 }
