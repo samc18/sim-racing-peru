a8188074f4a6dd02c087dd776c9b68e1e4b99298
Samuel Candela <93607710+samc18@users.noreply.github.com>
2023-02-07 21:09:34 -0500

    style: update menu items media queries

--- client/src/components/MenuItems.tsx
+++ client/src/components/MenuItems.tsx
@@ -11,27 +11,33 @@ const StyledMenuItems = styled.div`
     padding: 3em 0;
     display: grid;
     gap: 3em;
+    grid-template-areas:
+        "competiciones"
+        "pilotos"
+        "equipos"
+        "herramientas"
+        "pits";
+    ${StyledItem}:nth-child(1) {
+        grid-area: competiciones;
+    }
+    ${StyledItem}:nth-child(2) {
+        grid-area: herramientas;
+    }
+    ${StyledItem}:nth-child(3) {
+        grid-area: pilotos;
+    }
+    ${StyledItem}:nth-child(4) {
+        grid-area: equipos;
+    }
+    ${StyledItem}:nth-child(5) {
+        grid-area: pits;
+    }
 
     @media (min-width: ${breakPoints.tablet}) {
         grid-template-areas:
             "competiciones competiciones"
             "pilotos equipos"
             "herramientas pits";
-        ${StyledItem}:nth-child(1) {
-            grid-area: competiciones;
-        }
-        ${StyledItem}:nth-child(2) {
-            grid-area: herramientas;
-        }
-        ${StyledItem}:nth-child(3) {
-            grid-area: pilotos;
-        }
-        ${StyledItem}:nth-child(4) {
-            grid-area: equipos;
-        }
-        ${StyledItem}:nth-child(5) {
-            grid-area: pits;
-        }
         gap: 1em;
         padding-inline: ${margins.desktop};
     }
@@ -40,21 +46,6 @@ const StyledMenuItems = styled.div`
         grid-template-areas:
             "competiciones competiciones herramientas"
             "pilotos equipos pits";
-        ${StyledItem}:nth-child(1) {
-            grid-area: competiciones;
-        }
-        ${StyledItem}:nth-child(2) {
-            grid-area: herramientas;
-        }
-        ${StyledItem}:nth-child(3) {
-            grid-area: pilotos;
-        }
-        ${StyledItem}:nth-child(4) {
-            grid-area: equipos;
-        }
-        ${StyledItem}:nth-child(5) {
-            grid-area: pits;
-        }
         gap: 1em;
         padding-inline: ${margins.desktop};
 `
